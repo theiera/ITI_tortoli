c76253485e686494faa0b714efbae1ac30ae249a
Your Name <you@example.com>
2016-05-23 17:01:15 +0200

    bugfixes

--- sensore_umidit/sensore_umidit.ino
+++ sensore_umidit/sensore_umidit.ino
@@ -4,16 +4,17 @@ const int LIVELLO_UMIDITA = 250; // la soglia per accendere il LED
 
 void setup() {
      Serial.begin(9600);
+     pinMode(10, OUTPUT);
      }
 
 void LedState(int state) {
-     digitalWrite(13,state);
+     digitalWrite(10,state);
      }
 
 void loop() {
      int umidita = analogRead(VAL_PROBE);
      Serial.println(umidita);
-     if (umidita > LIVELLO_UMIDITA) {
+     if (umidita < LIVELLO_UMIDITA) {
      	LedState(HIGH);
 	} else {
 	  LedState(LOW);
